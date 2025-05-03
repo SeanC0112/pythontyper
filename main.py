a35e251c2cf8905088ec8fb0e797c792cedfb904
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2025-05-03 11:07:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-import keyboard
+from pynput import keyboard
+import keyboard as keyb
 from pandas.io.clipboard import clipboard_get
-if __name__ == "__main__":
-    while True:
-        #print("copy the text you would to write, then press 5. When ready to paste, press 6")
-        content = clipboard_get()
-        #keyboard.wait("e")
-        print("press 6 to write whenever ready")
-        keyboard.wait("1")
-        #keyboard.write(content)
+
+def on_release(key, injected):
+    print(key)
+    if key == keyboard.Key.esc:
+        # Stop listener
+        keyb.write(clipboard_get())
+        return False
+    if key == keyboard.Key.tab:
+        keyboard.press(keyboard.Key.backspace)
+        keyb.write(clipboard_get())
+        print("hi")
+
+def on_press(key, injected):
+    pass
+
+
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
